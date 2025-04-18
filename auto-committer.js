2cfe01ff24cffd609033f8067846250c0c35f219
alvaroescoto <alvaroedeavila@gmail.com>
2025-04-18 12:03:34 +0200

    updates

---
+++ auto-committer.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+// How often to commit (in minutes)
+const interval = 1;
+
+function autoCommit() {
+  const timestamp = new Date().toISOString();
+  const logPath = './log.txt';
+
+  // Add a new line to a file
+  fs.appendFileSync(logPath, `Auto commit at ${timestamp}\n`);
+
+  // Git commands
+  exec('git add . && git commit -m "Auto commit at ' + timestamp + '" && git push origin master', (err, stdout, stderr) => {
+    if (err) {
+      console.error('Commit error:', stderr);
+    } else {
+      console.log('Committed at:', timestamp);
+    }
+  });
+}
+
+// First run
+autoCommit();
+
+// Repeat every X minutes
+setInterval(autoCommit, interval * 60 * 1000);
